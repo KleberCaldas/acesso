7bf0511c7ac3a2bdf91f25cadf638de991d99086
Kleber Caldas <kleberlcaldas@gmail.com>
2021-02-04 00:01:15 -0300

    search updates

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
+import {Container, AreaInput, Input, List} from './styles';
+import Feather from 'react-native-vector-icons/Feather';
+import firestore from '@react-native-firebase/firestore';
+
 
 export default function Search(){
+    
+    const [input, setInput] = useState('');
+    const [places, setPlaces] = useState([]);
+    
+    useEffect(() => {
+        if(input === '' || input === undefined){
+            setPlaces([]);
+            return;
+        }
+
+        const subscriber = firestore().collection('RJ')
+        .where('name', '>=', input)
+        .where('name', '<=', input + "\uf8ff") //unicode
+        .onSnapshot( snapshot => {
+            const listPlaces = [];
+
+            snapshot.forEach( doc => {
+                listPlaces.push({
+                    ...doc.data(),
+                    id: doc.id
+                });
+            });
+            setPlaces(listPlaces);
+            console.log(listPlaces);
+        })
+
+        
+        return () => subscriber();
+
+    },[input]);
+    
     return(
-        <View>
-            <Text>Pagina de Search</Text>
-        </View>
+        <Container>
+            
+            <AreaInput>
+                <Feather
+                    name="search"
+                    color="#e52246"
+                    size={20}
+                />
+
+                <Input
+                    placeholder="Digite onde deseja ir"
+                    placeholderTextColor="#a9a9a9"
+                    value={input}
+                    onchangeText = {(text) => setInput(text)}
+                />
+
+            </AreaInput>
+        
+        </Container>
     );
 }
---
+++ src/pages/Search/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    padding-top: 15px;
+    flex: 1;
+    background-color: #a9a9a9;
+`;
+
+export const AreaInput = styled.View`
+    flex-direction: row;
+    margin: 10px;
+    background-color: #FFF
+    align-items: center;
+    padding: 5px 10px;
+    border-radius: 5px;
+`;
+
+export const Input = styled.TextInput`
+    width: 90%;
+    background-color: #FFF;
+    height: 40px;
+    padding-left: 8px;
+    font-size: 17px;
+    color: #121212;
+`;
+
+export const List = styled.FlatList`
+
+`;
