41f3a3b0371ee499932d4a592930dcc84d8a9996
Kleber Caldas <kleberlcaldas@gmail.com>
2021-06-22 14:30:56 -0300

    install and configuration API Google Maps

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function Maps() {
 
     return(
         <View>
-            <Text> Olá</Text>
+            <Text> Teste</Text>
+
+            <MapView
+                style={{width:350, height:350}}
+                initialRegion={{
+                    latitude:-22.9111689,
+                    longitude:-43.238321,
+                    latitudeDelta: 0.00922,
+                    longitudeDelta: 0.0421
+                }}
+            />
          </View>
         );
 
