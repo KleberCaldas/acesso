74dce3560abbc886178948b3545685196b474e76
Kleber Caldas <kleberlcaldas@gmail.com>
2021-06-25 21:40:50 -0300

    geolocation works

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,23 +1,108 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import MapView from 'react-native-maps';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import Feather from 'react-native-vector-icons/Feather';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
+
+export default class Maps extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            region:null
+        };
+    }
+
+    async componentDidMount(){
+        await Geolocation.getCurrentPosition(
+            async ({ coords:{latitude, longitude} })=>{
+                this.setState({
+                region:{
+                latitude,
+                longitude,
+                latitudeDelta:0.0922,
+                longitudeDelta:0.0421
+            }
+        });
+        console.log('oi')
+        },
+        (error)=>{ 
+            console.log(error.code, error.message)
+         },
+        {
+            enableHighAccuracy: true,
+            timeout:2000,
+            maximumAge:1000,
+            }
+        )
+    }
+
+    render() {
+        const {region} = this.state;
+        return (
+        <View style={styles.container}>
+  
+          <MapView
+            minZoomLevel={3}
+            style={styles.maps}
+            region={region}
+            showsUserLocation
+            loadingEnabled
+          />
+  
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    maps:{
+      width:'100%',
+      height:'100%',
+      marginTop: 15
+    }
+});
+
+
+
+/*
 export default function Maps() {
 
     return(
-        <View>
-            <Text> Teste</Text>
-
-            <MapView
-                style={{width:350, height:350}}
-                initialRegion={{
-                    latitude:-22.9111689,
-                    longitude:-43.238321,
+        <View style = {styles.container}>
+            <MapView 
+                style ={styles.maps}
+                region={{
+                    latitude: -22.9111689,
+                    longitude: -43.238321, 
                     latitudeDelta: 0.00922,
                     longitudeDelta: 0.0421
                 }}
-            />
-         </View>
-        );
+                showsUserLocation={true}
+            >
 
+
+            </MapView>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+    },
+    
+    maps:{
+        width: '100%',
+        height: '100%'
+    },
+
+});*/
